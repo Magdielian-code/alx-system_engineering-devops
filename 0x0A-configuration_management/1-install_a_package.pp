5334bfb5d86e2c4dd111689b4bbeb838ac80c381
magdielian-code <oziomaagaecheta81@gmail.com>
2024-03-22 16:53:43 +0100

    added file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,7 @@
 # Define the package resource
-package { 'Flask':
-  # Ensure the package is installed
-  ensure => present,
-  # Specify the package name
-  name => 'flask',
-  # Use pip3 as the provider
+package { 'flask':
+  ensure  => '2.1.0',
   provider => 'pip3',
-  # Specify the required version
-  require => ['package { python3 }'],  # Adjust if python3 is already installed
-  # Specify the exact version (optional)
-  ensure => '2.1.0',
 }
+
 
