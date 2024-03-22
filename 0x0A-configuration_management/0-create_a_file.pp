e807b3489ba3a8499e8030cd07129543b1abda80
magdielian-code <oziomaagaecheta81@gmail.com>
2024-03-22 16:25:05 +0100

    added file 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Define the file resource
+# This manifest creates a file at /tmp
 file { '/tmp/school':
-  ensure  => 'file',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
