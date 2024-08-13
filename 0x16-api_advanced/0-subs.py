3a8c5d77678da6cbde18a99b7f8115f7336c1d72
magdielian-code <oziomaagaecheta81@gmail.com>
2024-08-13 14:11:49 +0100

    fixed code error.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'ozioma'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     
     if response.status_code == 200:
